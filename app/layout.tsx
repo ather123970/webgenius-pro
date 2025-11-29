876c0b475fd932fb34bdc54539681bb0381ac718
Ather24-pixels <businessman2124377@gmail.com>
2025-11-29 04:15:15 -0800

    Add Google Analytics configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -126,6 +127,19 @@ export default function RootLayout({
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
             </head>
             <body className="antialiased">
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-SJM3ZKQCP2"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-SJM3ZKQCP2');
+                    `}
+                </Script>
                 {children}
             </body>
         </html>
